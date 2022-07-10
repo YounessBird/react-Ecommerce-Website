41be588e27dabe607ef2d0076b7d3f577f5dfe60
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-07-10 18:06:16 +0100

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./Header/Header";
-export { default as InfoSection } from "./InfoSection/InforSection";
-export { default as Pricing } from "./Pricing/Pricing";
-export { default as Footer } from "./Footer/Footer";
